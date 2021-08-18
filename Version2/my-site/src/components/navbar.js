77a12d0ceae1887fb43dd053f3155b353d7cba9f
Nadeem Howlader <55296425+nhowlade@users.noreply.github.com>
2021-08-18 01:45:56 -0400

    Adding scroll

--- Version2/my-site/src/components/navbar.js
+++ Version2/my-site/src/components/navbar.js
@@ -2,8 +2,14 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
+import { scroller } from "react-scroll";
 
 const navbar = () => {
+    const SmoothScroll = (name) => scroller.scrollTo(name, {
+        smooth: true,
+        offset: -70,
+        duration: 500,
+    })
     return (
         <Navbar collapseOnSelect sticky="top" variant="dark" bg="dark" expand="lg">
             <Container>
@@ -12,7 +18,7 @@ const navbar = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
                         <Nav.Link href="#introduction">Introduction</Nav.Link>
-                        <Nav.Link href="#md-team">The MD Team</Nav.Link>
+                        <Nav.Link onClick={() => SmoothScroll("md-team")}>The MD Team</Nav.Link>
                         <Nav.Link href="#my-role">My role in the team</Nav.Link>
                         <Nav.Link href="#goals">Goals and learnings</Nav.Link>
                         <Nav.Link href="#takeaways">Takeaways</Nav.Link>
--- Version2/my-site/src/components/paragraph.js
+++ Version2/my-site/src/components/paragraph.js
@@ -1,9 +1,9 @@
 import React from 'react'
 // import Jumbotron from 'react-bootstrap/Jumbotron'
 
-const Paragraph = ({title,content}) => {
+const Paragraph = ({title,content, name}) => {
     return (
-        <div>        
+        <div id={name}>        
             <h1>{title}</h1>
             {content.map(line =>(<p>{line}</p>))}
         </div>
