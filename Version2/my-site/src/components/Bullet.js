c6609961d73ef4bfb1c3a184f979069e5b84d327
Nadeem Howlader <55296425+nhowlade@users.noreply.github.com>
2021-09-07 13:17:12 -0400

    Adding the images for the goals

--- Version2/my-site/src/components/Bullet.js
+++ Version2/my-site/src/components/Bullet.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
 
-const Bullet = ({title,content}) => {
+const Bullet = ({title,content, imgsrc}) => {
     return (
         <div>        
             <h1>{title}</h1>
+            <Container>
+                <Row xs={1} lg={2}>
+                    <Col>{content.map((line,key) =>(<p key={key}>{line}</p>))}</Col>
+                    <Col ><Image src={imgsrc} fluid/></Col>
+                </Row>
+            </Container>
             {content.map((line,key) =>(<p key={key}>{line}</p>))}
         </div>
     )
