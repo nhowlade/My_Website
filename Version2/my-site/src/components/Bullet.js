9b56a8d715a55ce12bfaa611e5ef8885d217c877
Nadeem Howlader <55296425+nhowlade@users.noreply.github.com>
2021-09-12 22:15:48 -0400

    Added the ternary condition for the picture

--- Version2/my-site/src/components/Bullet.js
+++ Version2/my-site/src/components/Bullet.js
@@ -5,13 +5,16 @@ import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
 
 const Bullet = ({title,content, imgsrc}) => {
+    const arr = imgsrc.includes("sccm");
     return (
         <div>        
             <h1>{title}</h1>
             <Container>
-                <Row xs={1} lg={2}>
+                <Row xs={1} lg={2} className="align-items-center">
+                    {
+                        (arr === true ? <Col><Image src={imgsrc} fluid roundedCircle/></Col> :<Col><Image src={imgsrc} fluid/></Col>  )
+                    }
                     <Col>{content.map((line,key) =>(<p key={key}>{line}</p>))}</Col>
-                    <Col ><Image src={imgsrc} fluid/></Col>
                 </Row>
             </Container>
         </div>
